f1a0f06c93e9ad833957c09accadb4046bfcd210
amirshohsafoev <amirsafoev0@gmail.com>
2019-02-13 11:59:04 -0500

    wednesday morning

--- app/controllers/api/v1/user_exercises_controller.rb
+++ app/controllers/api/v1/user_exercises_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UserExercisesController < ApplicationController
   skip_before_action :authorized, only: [:index]
+  before_action :find_user_exercise, only: [:update, :destroy]
   def index
     @user_exercises = UserExercise.all
     render json: @user_exercises
@@ -9,17 +10,18 @@ class Api::V1::UserExercisesController < ApplicationController
     render json: @user_exercise
   end
   def update
+    # byebug
     @user_exercise.update(user_exercise_params)
     render json: @user_exercise
   end
   def destroy
-    @user_xercise = UserExercise.destroy
+    @user_exercise = UserExercise.destroy
     render json: {message: "removed"}, status: :ok
   end
 
   private
   def user_exercise_params
-    params.require(:user_exrcise).permit(
+    params.require(:user_exercise).permit(
       :reps,
       :sets,
       :weight,
